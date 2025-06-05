baffdce58e9aa0cc3d15bb15705abfb9f513c6fe
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-06-05 21:54:22 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:ddd/Acitivity/home.dart';
 import 'package:ddd/Acitivity/loading.dart';
 
-
-
 void main() {
   runApp(MaterialApp(
     routes: {
